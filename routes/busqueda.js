d5310399fd8530be58ceeb49ec08658c87b1cfc2
meruvia <gmeruviarojas@gmail.com>
2020-03-22 20:53:09 -0400

    Implementacion de busqueda con coincidencia en usuarios, medicos y hospitales

--- routes/busqueda.js
+++ routes/busqueda.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var Hospital = require('../models/Hospital');
 var Medico = require('../models/Medico');
+var Usuario = require('../models/usuario');
 
 var app = express();
 
@@ -10,11 +11,17 @@ app.get('/todo/:busqueda', (req, res, next) => {
     var busqueda = req.params.busqueda;
     var regex = new RegExp( busqueda, 'i');
 
-    buscarHospitales(busqueda, regex)
-        .then(hospitales => {
+    Promise.all([ 
+        buscarHospitales(busqueda, regex), 
+        buscarMedicos(busqueda, regex),
+        buscarUsuarios(busqueda, regex)
+        ])
+        .then( respuestas => {
             res.status(200).json({
                 ok: true,
-                hospitales: hospitales
+                hospitales: respuestas[0],
+                medicos: respuestas[1],
+                usuarios: respuestas[2]
             });
         });
 });
@@ -22,13 +29,45 @@ app.get('/todo/:busqueda', (req, res, next) => {
 // Funcion para la busqueda de hospitales con el uso de PROMESAS
 function buscarHospitales(busqueda, regex){
     return new Promise((resolve, reject) => {
-        Hospital.find({nombre: regex}, (err, hospitales) => {
+        Hospital.find({nombre: regex})
+            .populate('usuario', 'nombre email')
+            .exec((err, hospitales) => {
+                if(err){
+                    reject('Error al cargar hospitales', err);
+                }else{
+                    resolve(hospitales);
+                }
+            });
+    });
+}
+
+// Funcion para la busqueda de medicos con el uso de PROMESAS
+function buscarMedicos(busqueda, regex){
+    return new Promise((resolve, reject) => {
+        Medico.find({nombre: regex})
+        .populate('usuario', 'nombre email')
+        .populate('hospital')
+        .exec((err, medicos) => {
             if(err){
-                reject('Error al cargar hospitales', err);
+                reject('Error al cargar medicos', err);
             }else{
-                resolve(hospitales);
+                resolve(medicos);
             }
         } );
+    });
+}
+
+// Funcion para la busqueda de usuarios con el uso de PROMESAS
+function buscarUsuarios(busqueda, regex){
+    return new Promise((resolve, reject) => {
+        Usuario.find({}, 'nombre email role').or({'nombre': regex}, {'email': regex})
+        .exec( (err, usuarios) => {
+            if(err){
+                reject('Error al cargar usuarios', err);
+            }else{
+                resolve(usuarios);
+            }
+        });
     });
 }
 
